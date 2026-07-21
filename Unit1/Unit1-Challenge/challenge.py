2735241274f1ef84872c931572c6b483ee00b4cd
Chulumanco <manco.last@gmail.com>
2026-07-21 21:07:14 +0200

    Unit 1 - Challenge

---
+++ Unit1/Unit1-Challenge/challenge.py
@@ -0,0 +1,9 @@
+# 1. Ask the user for their name.
+name = input("Enter your name: ")
+
+# 2. Ask them for the name of the band/artist they want to see.
+artist = input("Enter the name of band/artist: ")
+
+# 3. Print a personalized confirmation message using an f-string that says something like: “Hey [Name]! Your tickets to see [Artist] are booked successfully!”
+
+print(f"Hey {name}! Your tickets to see {artist} are booked successfully!")
